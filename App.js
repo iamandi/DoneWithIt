e6ed2c0a5752f9d9959184d838db8233f98e1c3f
iamandi <aakawade9@gmail.com>
2020-07-08 01:44:05 -0700

    8- Improvements

--- App.js
+++ App.js
@@ -1,42 +1,7 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Dimensions,
-  SafeAreaView,
-  Switch,
-} from "react-native";
-import {
-  useDimensions,
-  useDeviceOrientation,
-} from "@react-native-community/hooks";
+import React from "react";
 
-import AppButton from "./components/AppButton";
-import AppText from "./components/AppText";
-import Card from "./components/Card";
-import Icon from "./components/Icon";
-import ListItem from "./components/ListItem";
-import Screen from "./components/Screen";
-import ListingDetailsScreen from "./screens/ListingDetailsScreen";
-import ViewImageScreen from "./screens/ViewImageScreen";
-import Messagesscreen from "./screens/MessagesScreen";
-import AccountScreen from "./screens/AccountScreen";
-import ListingScreen from "./screens/ListingScreen";
-import AppTextInput from "./components/AppTextInput";
-import AppPicker from "./components/AppPicker";
-import LoginScreen from "./screens/LoginScreen";
+import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 export default function App() {
-  return <Messagesscreen />;
+  return <ListingEditScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#f8f4f4",
-    padding: 20,
-    paddingTop: 30,
-  },
-});
